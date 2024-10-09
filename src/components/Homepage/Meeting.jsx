fb4627f20f9489896103a9aee0aa33161dbfdfc5
tejudharanesh <tejudharanesh1234@gmail.com>
2024-10-09 18:01:24 +0530

    added priority

--- src/components/Homepage/Meeting.jsx
+++ src/components/Homepage/Meeting.jsx
@@ -7,7 +7,7 @@ const Meeting = ({ show, onClose, handleVisitHabi, handleInviteHabi }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-20">
-      <div className="bg-white p-3 rounded-2xl text-center w-[361px] h-[268px] lg:w-[397px] lg:h-[250px] relative md:ml-40 lg:ml-60">
+      <div className="bg-white p-3 rounded-2xl text-center w-[361px] h-[268px] lg:w-[397px] lg:h-[270px] relative md:ml-40 lg:ml-60">
         <p className="mt-[27px] mb-[8px] text-gray-600 text-[12px] lg:text-[14px]">
           Meet our habi's executive at office.
         </p>
@@ -21,7 +21,7 @@ const Meeting = ({ show, onClose, handleVisitHabi, handleInviteHabi }) => {
           An executive from habi will visit your home.
         </p>
         <button
-          className=" bg-primary text-white  h-[60px] rounded-xl mx-auto w-[279px] lg:w-[315px]"
+          className="bg-primary text-white  h-[60px] rounded-xl mx-auto w-[279px] lg:w-[315px]"
           onClick={handleInviteHabi}
         >
           Invite habi
